6d7f47af8884e6aa2ff432e53d01027e3a0a6598
Elric Milon <whirm@gmx.com>
2015-06-08 13:16:54 +0200

    WIP STUFF

--- recipes-bsp/u-boot/files/boot.cmd
+++ recipes-bsp/u-boot/files/boot.cmd
@@ -2,3 +2,21 @@ setenv bootargs console=${console} console=tty1 root=/dev/mmcblk0p2 rootwait pan
 load mmc 0:1 ${fdt_addr_r} ${fdtfile} || load mmc 0:1 ${fdt_addr_r} boot/${fdtfile}
 load mmc 0:1 ${kernel_addr_r} zImage || load mmc 0:1 ${kernel_addr_r} boot/zImage || load mmc 0:1 ${kernel_addr_r} uImage || load mmc 0:1 ${kernel_addr_r} boot/uImage
 bootz ${kernel_addr_r} - ${fdt_addr_r} || bootm ${kernel_addr_r} - ${fdt_addr_r}
+
+#for location in zImage uImage boot/zImage boot/uImage; do;
+#    echo "LOCATION" ${location};
+#    if test -e mmc 0:1 ${location}; then;
+#       echo "EXISTS";
+#       load mmc 0:1 ${fdt_addr_r} ${prefix}${fdtfile};
+#       load mmc 0:1 ${kernel_addr_r} ${prefix}${kimage};
+#       if test ${kimage} = "zImage"; then;
+#          echo "ZIMAGE";
+#          bootz ${kernel_addr_r} - ${fdt_addr_r};
+#       else;
+#          echo "UIMAGE";
+#          bootm ${kernel_addr_r} - ${fdt_addr_r};
+#       fi;
+#     else;
+#       echo "NOT EXISTS";
+#    fi;
+#done;
